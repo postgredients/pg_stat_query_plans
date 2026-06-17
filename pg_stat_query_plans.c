ae4a50d897f73cd69a60d881ed331f417cc9688b
reshke <reshke@double.cloud>
2026-06-17 06:58:13 +0000

    Fix ABI change with 5a79e78501f4

--- pg_stat_query_plans.c
+++ pg_stat_query_plans.c
@@ -695,7 +695,9 @@ static void pgqp_ExecutorStart(QueryDesc *queryDesc, int eflags) {
       MemoryContext oldcxt;
 
       oldcxt = MemoryContextSwitchTo(queryDesc->estate->es_query_cxt);
-#if PG_VERSION_NUM >= 140000
+#if PG_VERSION_NUM >= 190000
+      queryDesc->totaltime = InstrAlloc(INSTRUMENT_ALL);
+#elif PG_VERSION_NUM >= 140000
       queryDesc->totaltime = InstrAlloc(1, INSTRUMENT_ALL, false);
 #else
       queryDesc->totaltime = InstrAlloc(1, INSTRUMENT_ALL);
